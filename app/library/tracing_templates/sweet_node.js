2b1cd412c2316eedb855adaa1f80a69a8bcea4e4
John Victor-Faichney <john.victor.faichney@gmail.com>
2014-06-27 18:15:49 -0700

    tweaks to tracing_templates

--- app/library/tracing_templates/sweet_node.js
+++ app/library/tracing_templates/sweet_node.js
@@ -9,18 +9,25 @@ var template_constructor = function(trace_json){
     material = new THREE.MeshLambertMaterial({ 
         color: "rgb(255,112,255)", 
         transparent: true, 
-        opacity: .5,
+        opacity: 0.2,
         shading: THREE.FlatShading, 
         vertexColors: THREE.VertexColors 
     });
         
     cube = new THREE.Mesh(geometry, material);
     cube.position.set(x, y, 0);
+    cube.renderDepth = 200;
     
     cube.animate = function(){
         this.rotation.x += 0.01;
 		this.rotation.y += 0.03;
     }
+    var tween = new TWEEN.Tween({ opac:  0.2 }).to({ opac: 1 }, 1000)
+    .delay(1000)
+    .onUpdate(function(){ 
+            cube.material.opacity = this.opac;
+        })
+    .start();
     
     cube.get_metadata = function(){
         return  trace_json.content;
--- app/library/tracing_templates/tweet_node.js
+++ app/library/tracing_templates/tweet_node.js
@@ -4,26 +4,29 @@ var template_constructor = function(trace_json){
      self.text = trace_json.text;
      var x = (trace_json.lat-22.28)*1000;
      var y = (trace_json.lon-114.15)*1000;
-    var a = new THREE.Vector3( 1, 1, 1 );
+
     
     var geometry = new THREE.BoxGeometry(1,1,1 );
     
     var material = new THREE.MeshLambertMaterial({ 
         color:  "rgb(255,112,255)", 
         transparent: true, 
-        opacity: 0.5,
+        opacity: 0.2,
         shading: THREE.FlatShading, 
         vertexColors: THREE.VertexColors 
     });
     
     var cube = new THREE.Mesh(geometry, material);
     cube.position.set(x, y, 0);
+    cube.renderDepth = 200;
     
+    console.log (cube);
      // Animation Methods/Tweens
-    var tween = new TWEEN.Tween({ x_size: 0.2 }).to({ x_size: 4 }, 5000)
+    var tween = new TWEEN.Tween({ x_size: 0.2, opac:  0.2 }).to({ x_size: 4, opac: 1 }, 1000)
+    .delay(1000)
     .onUpdate(function(){ 
             cube.scale.x = this.x_size;
-            console.log(cube.scale.x);
+            cube.material.opacity = this.opac;
         })
     .start();
      

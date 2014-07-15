ded6e1f2a47de70f906d6c07a35ec67983dbc6f8
cesandoval <blackbox@sghkcore.c.engaged-fact-636.internal>
2014-07-15 12:48:16 +0000

    update

--- app/js/utils/globalvar.js
+++ app/js/utils/globalvar.js
@@ -94,3 +94,27 @@ var SELECTED=[];
         mousedowncoords = {},
         marqueeGet = document.getElementById('select-marquee'),
         offset = {};
+
+
+
+var indexOf = function(needle) {
+    
+    if(typeof Array.prototype.indexOf === 'function') {
+        indexOf = Array.prototype.indexOf;
+    } else {
+        indexOf = function(needle) {
+            var i = -1, index = -1;
+
+            for(i = 0; i < this.length; i++) {
+                if(this[i] === needle) {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
+        };
+    }
+
+    return indexOf.call(this, needle);
+};
